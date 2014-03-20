d16c25f585dc85c05dc68827603b5463d0e62ee3
Igneel <dragon.ignil@gmail.com>
2014-03-20 17:34:04 +0700

    Пытаемся устроить тестовый бой. Пока неудачно.

--- Weapon.hs
+++ Weapon.hs
@@ -12,7 +12,7 @@ module Weapon where
 В каждый объект её встраивать видимо придется.
 
 Мысля: на карте мира хранить только существ, не умеющих двигаться, с их координатами.
-
+А вот координаты двигающихся существ будут храниться у них самих.
 Так, виды тайлов пока будут просто трава, земля, вода, воздух и лава:).
 
 Вспомогательные типы данных:
@@ -58,16 +58,20 @@ instance Show Tile where
 data Name = Name String
  deriving(Show,Eq)
 
-
-
-data Human = Human Name Coordinate
+data Health = Health Int
  deriving(Show,Eq)
+
+data Human = Human Name Health Coordinate
+ deriving(Eq)
+
+instance Show Human where
+	show (Human (Name s) _ _) = show $ head s
 
 instance HumanRace Human where
 	func = undefined
 
 instance Movable Human where
-	move (Human n c) cn = Human n cn
+	move (Human n h c) cn = Human n h cn
 
 instance Creature Human where
 	attack a b = undefined
@@ -78,11 +82,15 @@ class Movable a where
 
 class Creature a where -- класс существ
 	-- move::a->Point -- кто и куда двигается.
-	attack::(Weapon b)=>a->b->Point -- по идее существо которое бьёт хотя зачем..., чем бьём, куда бьём 
+	attack::(Weapon b)=>a->b->Coordinate->Bool -- по идее существо которое бьёт хотя зачем..., чем бьём, куда бьём 
+	attack a b (Coordinate (Point x y)) = undefined -- getDamage b
 
 class (Creature a,Movable a)=>HumanRace a where
 	func::a
 
+action = attack h1 knife (Coordinate (Point 0 3))
+
+knife = Knife (Damage 10) (Weight 0.1)
 
 main = putStrLn "Hello World"
 
@@ -92,7 +100,23 @@ data Point = Point Int Int
 data Coordinate = Coordinate Point
  deriving(Show,Eq)
 
+h1=Human (Name "Vaska") (Health 100) (Coordinate (Point 0 2))
+h2=Human (Name "Petka") (Health 100) (Coordinate (Point 0 3))
 
+creatures = [h1, h2]
+{-
+findCreatureByCoordinate c creatures = findCreatureByCoordinateHelper c creatures 0
+
+findCreatureByCoordinateHelper (Coordinate (Point x y)) [] position = Nothing
+findCreatureByCoordinateHelper (Coordinate (Point x y)) creatures position = 
+	if ((x,y) == getCoordinate $head creatures)
+	then Just position
+	else findCreatureByCoordinateHelper (Coordinate(Point x y)) (tail creatures) (position+1)
+
+
+getCoordinate::Human->(Int,Int)
+getCoordinate Human _ _ (Coordinate (Point x y)) = (x,y)
+-}
 data Distance = Distance Int
  deriving(Show,Eq)
 
@@ -104,6 +128,7 @@ instance Show World where
 	show (World x) = (show t) ++ (show $World $tail x)
 		where t = fst$ head x
 
+emptyMap width = concat $replicate width "X"
 worldMap::World
 worldMap = World [(Water,Coordinate (Point 0 0)),(Water,Coordinate (Point 0 1)), (Ground,Coordinate (Point 0 2)),
 	 (Ground,Coordinate (Point 1 0)),(Ground,Coordinate (Point 1 1)),(Ground,Coordinate (Point 1 2))]
@@ -118,6 +143,7 @@ data Weight = Weight Double
 class Weapon a where -- класс оружия
 	hit :: a ->Damage -- им можно бить, возвращает нанесенный урон
 	throw:: a->Distance -- его можно метать, возвращает как далеко удалось бросить.
+	getDamage::a->Int -- возвращает величину урона
 
 data Knife = Knife Damage Weight
  deriving(Show)
@@ -128,4 +154,5 @@ data Sword = Sword Damage Weight
 instance Weapon Knife where
 	hit (Knife d _) =  d 
 	throw (Knife _ w) = Distance $15
+	getDamage (Knife (Damage d) _) = d
 
